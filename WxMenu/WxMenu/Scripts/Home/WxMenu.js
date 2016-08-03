9cd2476d98b46a436d96f8d3ba4c0efca0ce8cf5
欧阳悲鸿 <lun3322@126.com>
2016-08-03 18:06:53 +0800

    完成一些界面逻辑

---
+++ WxMenu/WxMenu/Scripts/Home/WxMenu.js
@@ -0,0 +1,22 @@
+﻿var app = new Vue({
+    el: '#app',
+    data: {
+        menus: [
+
+        ]
+    },
+    methods: {
+        addMainMenu: function (event) {
+            // event 是dom事件对象
+            this.menus.push({ title: "菜单名称", subMenus: [] });
+        },
+        addSubMenu: function(index) {
+            this.menus[index].subMenus.push({ title: "子菜单名称" });
+        },
+        chooseMenu: function (event) {
+            event.stopPropagation();
+            $(".current").removeClass("current");
+            $(event.currentTarget).addClass("current");
+        }
+    }
+});
