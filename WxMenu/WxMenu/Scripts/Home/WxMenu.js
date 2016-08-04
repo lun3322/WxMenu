c8382285937a951ff79ed79e362ac2ba5c78051e
欧阳悲鸿 <lun3322@126.com>
2016-08-04 23:20:11 +0800

    设置框头部

--- WxMenu/WxMenu/Scripts/Home/WxMenu.js
+++ WxMenu/WxMenu/Scripts/Home/WxMenu.js
@@ -2,6 +2,11 @@
     el: '#app',
     data: {
         title: "测试公众号",
+        curMenu: {
+            title: "",
+            mainIndex: -1,
+            subIndex: -1
+        },
         menus: [
             {
                 title: "菜单名称",
@@ -19,12 +24,27 @@
             this.clearSelect();
             this.menus[index].subMenus.push({ title: "子菜单名称", select: true });
         },
+        deleteMenu: function (index, subIndex) {
+            this.clearSelect();
+            if (subIndex > -1) {
+                this.menus[index].subMenus.$remove(this.menus[index].subMenus[subIndex]);
+            } else {
+                this.menus.$remove(this.menus[index]);
+            }
+            this.chooseMenu(index);
+        },
         chooseMenu: function (index, subIndex) {
             this.clearSelect();
             if (subIndex != undefined) {
                 this.menus[index].subMenus[subIndex].select = true;
+                this.curMenu.title = this.menus[index].subMenus[subIndex].title;
+                this.curMenu.mainIndex = index;
+                this.curMenu.subIndex = subIndex;
             } else {
                 this.menus[index].select = true;
+                this.curMenu.title = this.menus[index].title;
+                this.curMenu.mainIndex = index;
+                this.curMenu.subIndex = -1;
             }
         },
         clearSelect: function () {
