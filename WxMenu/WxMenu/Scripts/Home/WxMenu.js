9095338e045f8428612708142e3f42fe361c5eda
欧阳悲鸿 <lun3322@126.com>
2016-08-04 15:30:33 +0800

    更新选中逻辑

--- WxMenu/WxMenu/Scripts/Home/WxMenu.js
+++ WxMenu/WxMenu/Scripts/Home/WxMenu.js
@@ -3,21 +3,37 @@
     data: {
         title: "测试公众号",
         menus: [
-
+            {
+                title: "菜单名称",
+                select: true,
+                subMenus: []
+            }
         ]
     },
     methods: {
-        addMainMenu: function (event) {
-            // event 是dom事件对象
-            this.menus.push({ title: "菜单名称", subMenus: [] });
+        addMainMenu: function () {
+            this.clearSelect();
+            this.menus.push({ title: "菜单名称", select: true, subMenus: [] });
         },
-        addSubMenu: function(index) {
-            this.menus[index].subMenus.push({ title: "子菜单名称" });
+        addSubMenu: function (index) {
+            this.clearSelect();
+            this.menus[index].subMenus.push({ title: "子菜单名称", select: true });
         },
-        chooseMenu: function (event) {
-            event.stopPropagation();
-            $(".current").removeClass("current");
-            $(event.currentTarget).addClass("current");
+        chooseMenu: function (index, subIndex) {
+            this.clearSelect();
+            if (subIndex != undefined) {
+                this.menus[index].subMenus[subIndex].select = true;
+            } else {
+                this.menus[index].select = true;
+            }
+        },
+        clearSelect: function () {
+            this.menus.forEach(function (value) {
+                value.select = false;
+                value.subMenus.forEach(function (subValue) {
+                    subValue.select = false;
+                });
+            });
         }
     }
 });
