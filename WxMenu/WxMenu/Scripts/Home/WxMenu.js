99f54e1c990bd07f5cb9853ca2e6e5989d47df84
欧阳悲鸿 <lun3322@126.com>
2016-08-17 23:34:44 +0800

    更新菜单设置逻辑

--- WxMenu/WxMenu/Scripts/Home/WxMenu.js
+++ WxMenu/WxMenu/Scripts/Home/WxMenu.js
@@ -10,23 +10,25 @@
             {
                 title: "菜单名称",
                 select: true,
+                action: "",
+                actionParam: "",
                 subMenus: []
             }
         ]
     },
     computed: {
-        curTitle: {
-            get: function () {
+        currentMenu: {
+            get:function() {
                 if (this.curMenu.subIndex > -1) {
-                    return this.menus[this.curMenu.mainIndex].subMenus[this.curMenu.subIndex].title;
+                    return this.menus[this.curMenu.mainIndex].subMenus[this.curMenu.subIndex];
                 }
-                return this.menus[this.curMenu.mainIndex].title;
+                return this.menus[this.curMenu.mainIndex];
             },
             set: function (newValue) {
                 if (this.curMenu.subIndex > -1) {
-                    this.menus[this.curMenu.mainIndex].subMenus[this.curMenu.subIndex].title = newValue;
+                    this.menus[this.curMenu.mainIndex].subMenus[this.curMenu.subIndex] = newValue;
                 } else {
-                    this.menus[this.curMenu.mainIndex].title = newValue;
+                    this.menus[this.curMenu.mainIndex] = newValue;
                 }
             }
         }
@@ -34,11 +36,13 @@
     methods: {
         addMainMenu: function () {
             this.clearSelect();
-            this.menus.push({ title: "菜单名称", select: true, subMenus: [] });
+            this.menus.push(this.getDefaultMenu(true));
         },
         addSubMenu: function (index) {
             this.clearSelect();
-            this.menus[index].subMenus.push({ title: "子菜单名称", select: true });
+            this.menus[index].subMenus.push(this.getDefaultMenu());
+            if (this.menus[index].action.length > 0) this.menus[index].action = "";
+            if (this.menus[index].actionParam.length > 0) this.menus[index].actionParam = "";
         },
         deleteMenu: function (index, subIndex) {
             this.clearSelect();
@@ -70,6 +74,27 @@
                     subValue.select = false;
                 });
             });
+        },
+        getDefaultMenu: function (isMainMenu) {
+            var menu = {
+                title: "子菜单名称",
+                select: true,
+                action: "",
+                actionParam: ""
+            };
+            if (isMainMenu) {
+                menu.subMenus = [];
+                menu.title = "菜单名称";
+            }
+            return menu;
+        },
+        showAction: function(menu) {
+            if (menu.hasOwnProperty('subMenus')) {
+                if (menu.subMenus.length > 0) {
+                    return false;
+                }
+            }
+            return true;
         }
     }
 });
